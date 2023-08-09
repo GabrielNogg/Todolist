6b4a2e85a6175f57d68aebd1cfd8c73d25691205
GabrielNog <gabrielnog.nda@gmail.com>
2023-08-09 14:28:42 -0300

    MVC Folders

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express = require('express');
-const app = express()
+const app = express();
 
-const port = 3000
+const port = 3000;
 
-app.listen(port)
+app.listen(port);
 
-app.get('/',(req,res)=>{
-    res.send('Hello World!')
-    req
-    console.log(req.baseUrl)
-    
-})
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.get('/ola',(req,res)=>{
-    res.send('Ola')
-    
-})
